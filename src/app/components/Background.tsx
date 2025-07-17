1424214379845fcd94d4c53a20a807f7160eb481
AzzADesigns <azariiielll@gmail.com>
2025-07-17 00:52:57 -0300

    [feat] TitlePage: added title to the  page

---
+++ src/app/components/Background.tsx
@@ -0,0 +1,11 @@
+import Spline from '@splinetool/react-spline/next';
+
+export default function Background() {
+return (
+    <main>
+      <Spline
+        scene="https://prod.spline.design/aNIDSkOimbLKkkzv/scene.splinecode" 
+      />
+    </main>
+  );
+}
